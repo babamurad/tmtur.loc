cda3213d39d6750bf25d28433b35f15e94cbc9e6
babamurad2010 <babamurad2010@yandex.ru>
2025-12-06 17:52:01 +0500

    Update posts-index.blade.php

--- resources/views/livewire/front/posts-index.blade.php
+++ resources/views/livewire/front/posts-index.blade.php
@@ -10,7 +10,7 @@
                     @endif
                     <div class="card-body">
                         <h2 class="card-title">{{ $post->tr('title') }}</h2>
-                        <div class="card-text">{!! Str::limit($post->tr('content'), 150) !!}</div>
+                        <p class="card-text">{!! Str::limit($post->tr('content'), 150) !!}</p>
                         
                         <a href="{{ route('blog.show', $post->slug) }}"
                             class="btn btn-primary read-more-btn">{{ __('messages.read_more') }}</a>
