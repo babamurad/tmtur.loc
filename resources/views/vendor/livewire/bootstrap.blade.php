21fd4cf6c69dd7e1e8a90963aaa481abfe590a16
Babamurad <babamurad2010@yandex.ru>
2026-01-09 11:22:36 +0500

    translate bootstrap pagination

---
+++ resources/views/vendor/livewire/bootstrap.blade.php
@@ -0,0 +1,97 @@
+<div>
+    @if ($paginator->hasPages())
+        <nav class="d-flex justify-content-between align-items-center justify-content-md-between">
+            <div class="d-flex justify-content-between flex-fill d-sm-none">
+                <ul class="pagination">
+                    {{-- Previous Page Link --}}
+                    @if ($paginator->onFirstPage())
+                        <li class="page-item disabled" aria-disabled="true">
+                            <span class="page-link">@lang('pagination.previous')</span>
+                        </li>
+                    @else
+                        <li class="page-item">
+                            <button type="button" wire:click="previousPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" class="page-link" rel="prev">@lang('pagination.previous')</button>
+                        </li>
+                    @endif
+
+                    {{-- Next Page Link --}}
+                    @if ($paginator->hasMorePages())
+                        <li class="page-item">
+                            <button type="button" wire:click="nextPage('{{ $paginator->getPageName() }}')"
+                                wire:loading.attr="disabled" class="page-link" rel="next">@lang('pagination.next')</button>
+                        </li>
+                    @else
+                        <li class="page-item disabled" aria-disabled="true">
+                            <span class="page-link">@lang('pagination.next')</span>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+
+            <div class="d-none flex-sm-fill d-sm-flex align-items-sm-center justify-content-sm-between">
+                <div>
+                    <p class="small text-muted">
+                        {!! __('pagination.showing', [
+            'first' => '<span class="fw-semibold">' . $paginator->firstItem() . '</span>',
+            'last' => '<span class="fw-semibold">' . $paginator->lastItem() . '</span>',
+            'total' => '<span class="fw-semibold">' . $paginator->total() . '</span>',
+        ]) !!}
+                    </p>
+                </div>
+
+                <div>
+                    <ul class="pagination">
+                        {{-- Previous Page Link --}}
+                        @if ($paginator->onFirstPage())
+                            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.previous')">
+                                <span class="page-link" aria-hidden="true">&lsaquo;</span>
+                            </li>
+                        @else
+                            <li class="page-item">
+                                <button type="button" class="page-link"
+                                    wire:click="previousPage('{{ $paginator->getPageName() }}')" wire:loading.attr="disabled"
+                                    rel="prev" aria-label="@lang('pagination.previous')">&lsaquo;</button>
+                            </li>
+                        @endif
+
+                        {{-- Pagination Elements --}}
+                        @foreach ($elements as $element)
+                            {{-- "Three Dots" Separator --}}
+                            @if (is_string($element))
+                                <li class="page-item disabled" aria-disabled="true"><span class="page-link">{{ $element }}</span>
+                                </li>
+                            @endif
+
+                            {{-- Array Of Links --}}
+                            @if (is_array($element))
+                                @foreach ($element as $page => $url)
+                                    @if ($page == $paginator->currentPage())
+                                        <li class="page-item active" aria-current="page"><span class="page-link">{{ $page }}</span></li>
+                                    @else
+                                        <li class="page-item"><button type="button" class="page-link"
+                                                wire:click="gotoPage({{ $page }}, '{{ $paginator->getPageName() }}')">{{ $page }}</button>
+                                        </li>
+                                    @endif
+                                @endforeach
+                            @endif
+                        @endforeach
+
+                        {{-- Next Page Link --}}
+                        @if ($paginator->hasMorePages())
+                            <li class="page-item">
+                                <button type="button" class="page-link" wire:click="nextPage('{{ $paginator->getPageName() }}')"
+                                    wire:loading.attr="disabled" rel="next"
+                                    aria-label="@lang('pagination.next')">&rsaquo;</button>
+                            </li>
+                        @else
+                            <li class="page-item disabled" aria-disabled="true" aria-label="@lang('pagination.next')">
+                                <span class="page-link" aria-hidden="true">&rsaquo;</span>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    @endif
+</div>
