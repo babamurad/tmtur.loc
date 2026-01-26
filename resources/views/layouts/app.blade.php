e5f7a0326378080802e1d0c5adfa514a0a91dba6
Babamurad <babamurad2010@yandex.ru>
2026-01-26 14:04:49 +0500

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -309,7 +309,7 @@
             <div class="container-fluid">
                 <div class="row">
                     <div class="col-sm-6">
-                        2025 © Opatix.
+                        {{ date('Y') }} © TMTourism.
                     </div>
                     <div class="col-sm-6">
                         <div class="text-sm-right d-none d-sm-block">
