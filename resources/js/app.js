071302c217d959b2ebf0d2a4f22fc9326d44ab42
Babamurad <babamurad2010@yandex.ru>
2026-02-07 12:16:12 +0500

    dropdown решение

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,10 +2,10 @@
 import jQuery from 'jquery';
 window.$ = window.jQuery = jQuery;
 
-import Popper from 'popper.js';
-window.Popper = Popper;
+// import Popper from 'popper.js';
+// window.Popper = Popper;
 
-import 'bootstrap';
+// import 'bootstrap';
 
 import Swal from 'sweetalert2';
 window.Swal = Swal;
